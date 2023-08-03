760b92aacb77c373377ab82cf50841de2f497958
Jeff Decker <jeff@jeffcd.com>
2023-08-03 13:29:12 -0700

    add generic heap to codeapro

--- codeapro.d.ts
+++ codeapro.d.ts
@@ -1,3 +1,27 @@
+declare class GenericHeap<T> {
+  private heap: Array<T>;
+  private entryMap: Record<number, number>;
+  private comparator: (a: T, b: T) => boolean;
+  private getEntryIndex: (entry: T) => number;
+  constructor({
+    array,
+    comparator = (a, b) => a < b,
+    getEntryIndex = (entry) => entry as number,
+  }: {
+    array: Array<T>;
+    comparator: (a: T, b: T) => boolean;
+    getEntryIndex: (entry: T) => number;
+  }): void;
+  public BuildHeap(): void;
+  public SiftDown(currentI: number): void;
+  public SiftUp(currentI: number): void;
+  public update(entryIndex: number, handler: (entry: T) => T);
+  public peek(): T;
+  public remove(): T;
+  public insert(value: T);
+  public isEmpty(): boolean;
+  private swap(a: number, b: number): void;
+}
 declare function generateSingleLinkedList<T>(array: Array<T>);
 
-export { generateSingleLinkedList };
+export { GenericHeap, generateSingleLinkedList };
--- mainSrc/index.ts
+++ mainSrc/index.ts
@@ -1,3 +1,112 @@
+class GenericHeap<T> {
+  heap: Array<T>;
+  entryMap: Record<number, number>;
+  comparator: (a: T, b: T) => boolean;
+  getEntryIndex: (entry: T) => number;
+
+  constructor({
+    array,
+    comparator = (a, b) => a < b,
+    getEntryIndex = (entry) => entry as number,
+  }: {
+    array: Array<T>;
+    comparator: (a: T, b: T) => boolean;
+    getEntryIndex: (entry: T) => number;
+  }) {
+    this.entryMap = array.reduce(
+      (acc, _, index) => {
+        acc[index] = index;
+        return acc;
+      },
+      {} as Record<number, number>,
+    );
+    this.heap = array;
+    this.comparator = comparator;
+    this.getEntryIndex = getEntryIndex;
+    this.buildHeap();
+  }
+
+  buildHeap() {
+    const midI = Math.floor((this.heap.length - 2) / 2);
+    for (let currentI = midI; currentI >= 0; currentI--) {
+      this.siftDown(currentI);
+    }
+  }
+
+  siftDown(currentI = 0) {
+    const endI = this.heap.length - 1;
+    let childOneI = currentI * 2 + 1;
+    while (childOneI <= endI) {
+      let smallestChildI = childOneI;
+      const childTwoI = childOneI + 1 <= endI ? childOneI + 1 : -1;
+      if (
+        childTwoI !== -1 &&
+        this.comparator(this.heap[childTwoI], this.heap[childOneI])
+      ) {
+        smallestChildI = childTwoI;
+      }
+      if (this.comparator(this.heap[smallestChildI], this.heap[currentI])) {
+        this.swap(smallestChildI, currentI);
+        currentI = smallestChildI;
+        childOneI = currentI * 2 + 1;
+      } else {
+        return;
+      }
+    }
+  }
+
+  siftUp(currentI = this.heap.length - 1) {
+    let parentI = Math.floor((currentI - 1) / 2);
+    while (
+      currentI > 0 &&
+      this.comparator(this.heap[currentI], this.heap[parentI])
+    ) {
+      this.swap(currentI, parentI);
+      currentI = parentI;
+      parentI = Math.floor((currentI - 1) / 2);
+    }
+  }
+
+  update(entryIndex: number, handler: (entry: T) => T) {
+    this.heap[this.entryMap[entryIndex]] = handler(
+      this.heap[this.entryMap[entryIndex]],
+    );
+    this.siftUp(this.entryMap[entryIndex]);
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  remove() {
+    if (this.isEmpty()) {
+      return;
+    }
+    this.swap(0, this.heap.length - 1);
+    const removedValue: T = this.heap.pop() as T;
+    delete this.entryMap[this.getEntryIndex(removedValue)];
+    this.siftDown();
+    return removedValue;
+  }
+
+  insert(value: T) {
+    this.heap.push(value);
+    this.siftUp();
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  swap(a: number, b: number) {
+    this.entryMap[this.getEntryIndex(this.heap[a])] = b;
+    this.entryMap[this.getEntryIndex(this.heap[b])] = a;
+    const tmp = this.heap[a];
+    this.heap[a] = this.heap[b];
+    this.heap[b] = tmp;
+  }
+}
+
 function generateSingleLinkedList<T>(array: Array<T>) {
   let head: SingleLinkedListType = null;
   let node: SingleLinkedListType = null;
@@ -14,4 +123,4 @@ function generateSingleLinkedList<T>(array: Array<T>) {
   return head;
 }
 
-export { generateSingleLinkedList };
+export { GenericHeap, generateSingleLinkedList };
