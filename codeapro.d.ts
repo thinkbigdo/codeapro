3fc1810fc1c85acc92b661b7a5bfde4f765141bc
Jeff Decker <jeff@jeffcd.com>
2023-08-05 08:42:26 -0700

    update generic heap to maintain length internally and ability to removeToEnd (useful for heapSort)

--- codeapro.d.ts
+++ codeapro.d.ts
@@ -3,6 +3,7 @@ declare class GenericHeap<T> {
   private entryMap: Record<number, number>;
   private comparator: (a: T, b: T) => boolean;
   private getEntryIndex: (entry: T) => number;
+  private useEntryMap: boolean;
   constructor({
     array,
     comparator = (a, b) => a < b,
@@ -20,6 +21,7 @@ declare class GenericHeap<T> {
   public remove(): T;
   public insert(value: T);
   public isEmpty(): boolean;
+  public getLength(): number;
   private swap(a: number, b: number): void;
 }
 declare function generateSingleLinkedList<T>(array: Array<T>);
--- mainSrc/index.ts
+++ mainSrc/index.ts
@@ -3,38 +3,48 @@ class GenericHeap<T> {
   entryMap: Record<number, number>;
   comparator: (a: T, b: T) => boolean;
   getEntryIndex: (entry: T) => number;
+  length: number;
+  useEntryMap: boolean;
 
   constructor({
     array,
     comparator = (a, b) => a < b,
     getEntryIndex = (entry) => entry as number,
+    useEntryMap = false,
   }: {
     array: Array<T>;
     comparator: (a: T, b: T) => boolean;
-    getEntryIndex: (entry: T) => number;
+    getEntryIndex?: (entry: T) => number;
+    useEntryMap: boolean;
   }) {
-    this.entryMap = array.reduce(
-      (acc, _, index) => {
-        acc[index] = index;
-        return acc;
-      },
-      {} as Record<number, number>,
-    );
+    if (useEntryMap) {
+      this.entryMap = array.reduce(
+        (acc, _, index) => {
+          acc[index] = index;
+          return acc;
+        },
+        {} as Record<number, number>,
+      );
+    } else {
+      this.entryMap = {};
+    }
+    this.length = array.length;
     this.heap = array;
     this.comparator = comparator;
     this.getEntryIndex = getEntryIndex;
+    this.useEntryMap = useEntryMap;
     this.buildHeap();
   }
 
   buildHeap() {
-    const midI = Math.floor((this.heap.length - 2) / 2);
+    const midI = Math.floor((this.length - 2) / 2);
     for (let currentI = midI; currentI >= 0; currentI--) {
       this.siftDown(currentI);
     }
   }
 
   siftDown(currentI = 0) {
-    const endI = this.heap.length - 1;
+    const endI = this.length - 1;
     let childOneI = currentI * 2 + 1;
     while (childOneI <= endI) {
       let smallestChildI = childOneI;
@@ -55,7 +65,7 @@ class GenericHeap<T> {
     }
   }
 
-  siftUp(currentI = this.heap.length - 1) {
+  siftUp(currentI = this.length - 1) {
     let parentI = Math.floor((currentI - 1) / 2);
     while (
       currentI > 0 &&
@@ -68,40 +78,70 @@ class GenericHeap<T> {
   }
 
   update(entryIndex: number, handler: (entry: T) => T) {
-    this.heap[this.entryMap[entryIndex]] = handler(
-      this.heap[this.entryMap[entryIndex]],
-    );
-    this.siftUp(this.entryMap[entryIndex]);
+    if (this.useEntryMap) {
+      this.heap[this.entryMap[entryIndex]] = handler(
+        this.heap[this.entryMap[entryIndex]],
+      );
+    } else {
+      this.heap[entryIndex] = handler(this.heap[entryIndex]);
+    }
+    this.siftUp(entryIndex);
   }
 
   peek() {
     return this.heap[0];
   }
 
+  removeToEnd() {
+    if (this.isEmpty()) {
+      return;
+    }
+    this.swap(0, this.length - 1);
+    const removedValue: T = this.heap[this.length - 1] as T;
+    if (this.useEntryMap) {
+      delete this.entryMap[this.getEntryIndex(removedValue)];
+    }
+    this.length -= 1;
+    this.siftDown();
+    return removedValue;
+  }
+
   remove() {
     if (this.isEmpty()) {
       return;
     }
-    this.swap(0, this.heap.length - 1);
+    this.swap(0, this.length - 1);
     const removedValue: T = this.heap.pop() as T;
-    delete this.entryMap[this.getEntryIndex(removedValue)];
+    if (this.useEntryMap) {
+      delete this.entryMap[this.getEntryIndex(removedValue)];
+    }
+    this.length -= 1;
     this.siftDown();
     return removedValue;
   }
 
   insert(value: T) {
     this.heap.push(value);
-    this.entryMap[this.getEntryIndex(value)] = this.heap.length - 1;
+    if (this.useEntryMap) {
+      this.entryMap[this.getEntryIndex(value)] = this.length - 1;
+    }
+    this.length += 1;
     this.siftUp();
   }
 
   isEmpty() {
-    return this.heap.length === 0;
+    return this.length === 0;
+  }
+
+  getLength() {
+    return this.length;
   }
 
   swap(a: number, b: number) {
-    this.entryMap[this.getEntryIndex(this.heap[a])] = b;
-    this.entryMap[this.getEntryIndex(this.heap[b])] = a;
+    if (this.useEntryMap) {
+      this.entryMap[this.getEntryIndex(this.heap[a])] = b;
+      this.entryMap[this.getEntryIndex(this.heap[b])] = a;
+    }
     const tmp = this.heap[a];
     this.heap[a] = this.heap[b];
     this.heap[b] = tmp;
